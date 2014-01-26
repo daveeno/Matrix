3d5f911980a287118ef26736cae1ec171ef1da74
David Eno <daveeno@gmail.com>
2014-01-25 19:07:32 -0500

    Initial Commit
    Initial commit

---
+++ Matrix.cs
@@ -0,0 +1,450 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MatVec
+{
+  public class Matrix
+  {
+    double[,] _M;
+    int _rows, _cols;
+
+    public Matrix(int r, int c)
+    {
+      _M = new double[r, c];
+      _rows = r;
+      _cols = c;
+      for(int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+          _M[i, j] = 0;
+        }
+      }
+    }
+
+    public Matrix(int r) // Makes Identity Matrix
+    {
+      _M = new double[r, r];
+      _rows = r;
+      _cols = r;
+      for (int i = 0; i < r; i++) {
+        for (int j = 0; j < r; j++) {
+          if (i == j)
+            _M[i, j] = 1;
+          else
+            _M[i, j] = 0;
+        }
+      }
+    }
+
+    public Matrix(string matstr)
+    {
+      string[] rows = matstr.Split(';');
+      string[] cols = rows[0].Split(',');
+      _rows = rows.Count();
+      _cols = cols.Count();
+      _M = new double[_rows, _cols];
+
+      for (int i = 0; i < _rows; i++ )
+      {
+        cols = rows[i].Split(',');
+        for (int j = 0; j < _cols; j++)
+        {
+          _M[i, j] = double.Parse(cols[j]);
+        }
+      }
+    }
+
+    public Matrix(string[] rows)
+    {
+      string[] cols = rows[0].Split(' ');
+      _rows = rows.Count();
+      _cols = cols.Count();
+      //MessageBox.Show(_rows + ", " + _cols);
+      _M = new double[_rows, _cols];
+
+      for (int i = 0; i < _rows; i++)
+      {
+        cols = rows[i].Split(' ');
+        for (int j = 0; j < _cols; j++)
+        {
+          _M[i, j] = double.Parse(cols[j]);
+        }
+      }
+    }
+
+    public static Matrix operator +(Matrix m1, Matrix m2)
+    {
+      if (m1.getRows() != m2.getRows())
+        return null;
+      if (m1.getCols() != m2.getCols())
+        return null;
+      Matrix dm = new Matrix(m1.getRows(), m1.getCols());
+      for (int i = 0; i < m1.getRows(); i++)
+      {
+        for (int j = 0; j < m1.getCols(); j++)
+        {
+          dm.setVal(i, j, m1.getVal(i, j) + m2.getVal(i, j));
+        }
+      }
+      return dm;
+    }
+
+    public static Matrix operator -(Matrix m1, Matrix m2)
+    {
+      if (m1.getRows() != m2.getRows())
+        return null;
+      if (m1.getCols() != m2.getCols())
+        return null;
+      Matrix dm = new Matrix(m1.getRows(), m1.getCols());
+      for (int i = 0; i < m1.getRows(); i++)
+      {
+        for (int j = 0; j < m1.getCols(); j++)
+        {
+          dm.setVal(i, j, m1.getVal(i, j) - m2.getVal(i, j));
+        }
+      }
+      return dm;
+    }
+
+    public static Matrix operator *(Matrix m1, Matrix m2)
+    {
+      Matrix MN = new Matrix(m1.getRows(), m2.getCols());
+      int i, j, k;
+
+      for (i = 0; i < m1.getRows(); i++)
+      {
+        for (j = 0; j < m2.getCols(); j++)
+        {
+          for (k = 0; k < m2.getRows(); k++)
+          {
+            MN.setVal(i, j, MN.getVal(i, j) + m1.getVal(i,k) * m2.getVal(k, j));
+          }
+        }
+      }
+
+      return MN;
+    }
+
+    public static Matrix operator *(double d, Matrix m1)
+    {
+      Matrix MN = new Matrix(m1.getRows(), m1.getCols());
+      int i, j;
+
+      for (i = 0; i < m1.getRows(); i++)
+      {
+        for (j = 0; j < m1.getCols(); j++)
+        {
+          MN.setVal(i, j, m1.getVal(i, j) * d);
+        }
+      }
+
+      return MN;
+    }
+
+    public void setVal(int r, int c, double val)
+    {
+      this._M[r, c] = val;
+    }
+
+    public double getVal(int r, int c)
+    {
+      return this._M[r, c];
+    }
+
+    public int getRows()
+    {
+      return _rows;
+    }
+
+    public int getCols()
+    {
+      return _cols;
+    }
+
+    public void swapRows(int r1, int r2)
+    {
+      for (int i = 0; i < this._cols; i++) {
+        var t = this._M[r1, i];
+        this._M[r1, i] = this._M[r2, i];
+        this._M[r2, i] = t;
+      }
+    }
+
+    public Matrix transpose()
+    {
+      Matrix N = new Matrix(this._cols, this._rows);
+
+      for (int i = 0; i < this._rows; i++) {
+        for (int j = 0; j < this._cols; j++) {
+          N.setVal(j, i, this._M[i, j]);
+        }
+      }
+      return N;
+    }
+
+    public bool isInvertable()
+    {
+      try
+      {
+        this.inverse();
+        return true;
+      }
+      catch
+      {
+        return false;
+      }
+    }
+
+    public Matrix inverse()
+    {
+      if (_rows != _cols) return null;
+
+      int i, j;
+      Matrix a = new Matrix(this._rows, this._cols);
+
+      Matrix N;
+      N = this.augment(new Matrix(this._rows));
+      N.rref();
+      for (i = 0; i < this._rows; i++)
+      {
+        for (j = 0; j < this._cols; j++)
+        {
+          a.setVal(i, j, N.getVal(i, j + N.getRows()));
+        }
+      }
+      return a;
+    }
+
+    public Matrix augment(Matrix N)
+    {
+      int i, j;
+      Matrix a = new Matrix(this._rows, this._cols + N.getCols());
+      for (i = 0; i < this._rows; i++) {
+        for (j = 0; j < this._cols; j++) {
+          a.setVal(i, j, _M[i,j]);
+        }
+        for (j = 0; j < N.getCols(); j++) {
+          a.setVal(i, j + this._rows, N.getVal(i, j));
+        }
+      }
+      return a;
+    }
+
+    public Matrix lastNCols(int col)
+    {
+      Matrix M = new Matrix(this._rows, col);
+      int k = 0;
+      for(int j = 0; j < this._rows; j++)
+      {
+        for(int i = this._cols - col; i < this._cols; i++)
+        {
+          M.setVal(j, k, this._M[j, i]);
+          k++;
+        }
+        k = 0;
+      }
+      
+      return M;
+    }
+
+    public void rref()
+    {
+      int i, j, r;
+
+      int lead = 0;
+      for (r = 0; r < this._rows; r++)
+      {
+        if (lead >= this._cols)
+          break;
+        {
+          i = r;
+          while (_M[i, lead] == 0)
+          {
+            i++;
+            if (i == this._rows)
+            {
+              i = r;
+              lead++;
+              if (lead == this._cols)
+                break;
+            }
+          }
+          swapRows(r, i);
+        }
+
+        double lv = _M[r, lead];
+        for (j = 0; j < this._cols; j++)
+          _M[r, j] = _M[r, j] / lv;
+
+        for (i = 0; i < this._rows; i++)
+        {
+          if (i != r)
+          {
+            lv = _M[i, lead];
+            for (j = 0; j < this._cols; j++)
+              _M[i, j] -= lv * _M[r, j];
+          }
+        }
+        lead++;
+      }
+    }
+
+    public Matrix solve(int solncols)
+    {
+      if (this._rows >= this._cols)
+        return null;
+
+      Matrix soln = new Matrix(this._rows, solncols);
+
+      int i, j, k, p;
+      int n = this._rows;
+
+      for (i = 0; i <= n - 2; i++)
+      {
+        p = i;
+        while (_M[p,i] == 0 && p < n - 1)
+          p++;
+        if (p == n - 1)
+        {
+          return null;
+        }
+        if (p != i)
+        {
+          swapRows(p, i);
+        }
+        double m_ji;
+        for (j = i + 1; j < n; j++)
+        {
+          m_ji = _M[j,i] / _M[i,i];
+          for (k = i; k <= n; k++)
+          {
+            _M[j,k] -= m_ji * _M[i,k];
+          }
+          _M[j,i] = 0;
+        }
+      }
+
+      double sum = 0;
+      double[] x;
+      x = new double[n];
+
+      x[n - 1] = _M[n - 1,n] / _M[n - 1,n - 1];
+      for (i = n - 2; i >= 0; i--)
+      {
+        for (j = i + 1; j <= n - 1; j++)
+        {
+          sum += _M[i,j] * x[j];
+        }
+        x[i] = (_M[i,n] - sum) / _M[i,i];
+        sum = 0;
+      }
+
+      // Output
+      for (k = 0; k < n; k++)
+      {
+        soln.setVal(k,0, x[k]);
+      }
+      return soln;
+    }
+
+    public Matrix mmult(Matrix N)
+    {
+      Matrix MN = new Matrix(this._rows, N.getCols());
+      int i, j, k;
+
+      for (i = 0; i < this._rows; i++) {
+        for (j = 0; j < N.getCols(); j++) {
+          for (k = 0; k < N.getRows(); k++) {
+            MN.setVal(i, j, MN.getVal(i, j) + this._M[i,k] * N.getVal(k, j));
+          }
+        }
+      }
+
+      return MN;
+    }
+
+    public String toStr()
+    {
+      return this.toStr(0);
+    }
+
+    public String toStr(int decplcs)
+    {
+      String strout = "";
+      int i, j;
+
+      for (i = 0; i < this._rows; i++)
+      {
+        //strout += "[ ";
+        for (j = 0; j < this._cols; j++)
+        {
+          if (decplcs > 0) strout = strout + Math.Round(_M[i, j], decplcs) + " ";
+          else strout = strout + _M[i, j] + " ";
+        }
+        strout = strout + "\r\n";
+      }
+
+      return strout;
+    }
+
+    public string toMatStr()
+    {
+      String strout = "";
+      int i, j;
+
+      for (i = 0; i < this._rows; i++)
+      {
+        //strout += "[ ";
+        for (j = 0; j < this._cols; j++)
+        {
+          strout = strout + _M[i, j];
+          if(j < this._cols-1) strout += ",";
+        }
+        if(i < this._rows - 1) strout = strout + ";";
+      }
+
+      return strout;
+    }
+
+    private double fix(double n, double dp)
+    {
+      if (dp > 0)
+      {
+        double prec = Math.Pow(10, dp);
+        double num = Math.Round(n * dp);
+        return num / prec;
+      }
+      return n;
+    }
+
+    public void addRows(int n)
+    {
+      this._rows += n;
+      
+    }
+
+
+    public double DET()
+    {
+      int i, j, k;
+      double det = 0;
+      int n = _rows;
+      for (i = 0; i < n - 1; i++)
+      {
+        for (j = i + 1; j < n; j++)
+        {
+          det = _M[j, i] / _M[i, i];
+          for (k = i; k < n; k++)
+            _M[j, k] = _M[j, k] - det * _M[i, k];
+        }
+      }
+      det = 1;
+      for (i = 0; i < n; i++)
+        det = det * _M[i, i];
+      return det;
+    }
+
+  } // Matrix
+}
---
+++ Vector.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatVec
+{
+  public class Vector
+  {
+    private double[] _V;
+    public double[] Components
+    {
+      get { return _V; }
+    }
+
+    private int _rows;
+    public int Rows
+    {
+      get { return _rows; }
+    }
+
+    public Vector(int n)
+    {
+      _rows = n;
+      _V = new double[_rows];
+    }
+
+    public Vector(double[] comps)
+    {
+      _rows = comps.Count();
+      _V = new double[_rows];
+      for (int i = 0; i < _rows; i++)
+        _V[i] = comps[i];
+    }
+
+    public Vector(string[] rows)
+    {
+      _rows = rows.Count();
+      _V = new double[_rows];
+      for (int i = 0; i < _rows; i++)
+        _V[i] = double.Parse(rows[i]);
+    }
+
+    public Vector(string vecstr)
+    {
+      string[] rows = vecstr.Split('\n');
+      _rows = rows.Count();
+      _V = new double[_rows];
+
+      for (int i = 0; i < _rows; i++)
+      {
+        _V[i] = double.Parse(rows[i]);
+      }
+    }
+
+    public static Vector operator +(Vector v1, Vector v2)
+    {
+      if (v1.Rows == v2.Rows)
+      {
+        double[] d = new double[v1.Rows];
+        for(int i = 0; i < v1.Rows; i++)
+        {
+          d[i] = v1.Components[i] + v2.Components[i];
+        }
+        return new Vector(d);
+      }
+      return null;
+    }
+
+    public static Vector operator -(Vector v1, Vector v2)
+    {
+      if (v1.Rows == v2.Rows)
+      {
+        double[] d = new double[v1.Rows];
+        for (int i = 0; i < v1.Rows; i++)
+        {
+          d[i] = v1.Components[i] - v2.Components[i];
+        }
+        return new Vector(d);
+      }
+      return null;
+    }
+
+    public static Vector operator *(Matrix m1, Vector v1)
+    {
+      Vector mv = new Vector(m1.getRows());
+
+      mv.Components[0] = v1.Components[0] * m1.getVal(0, 0) + v1.Components[1] * m1.getVal(0, 1) + v1.Components[2] * m1.getVal(0, 2);
+      mv.Components[1] = v1.Components[0] * m1.getVal(1, 0) + v1.Components[1] * m1.getVal(1, 1) + v1.Components[2] * m1.getVal(1, 2);
+      mv.Components[2] = v1.Components[0] * m1.getVal(2, 0) + v1.Components[1] * m1.getVal(2, 1) + v1.Components[2] * m1.getVal(2, 2);
+
+      return mv;
+    }
+
+    public Vector SMult(double s)
+    {
+      double[] d = new double[_rows];
+      for (int i = 0; i < _rows; i++)
+      {
+        d[i] = _V[i] * s;
+      }
+      return new Vector(d);
+    }
+
+    public double Dotp(Vector v)
+    {
+      double d = 0;
+      if (_rows == v.Rows)
+      {
+        for (int i = 0; i < _rows; i++)
+        {
+          d += _V[i] * v.Components[i];
+        }
+      }
+      return d;
+    }
+
+    public Vector Crossp(Vector v)
+    {
+      double[] d = new double[3];
+      if (_rows == 3 && _rows == v.Rows)
+      {
+        d[0] = _V[1] * v.Components[2] + _V[2] * v.Components[1];
+        d[1] = _V[0] * v.Components[2] + _V[2] * v.Components[0];
+        d[2] = _V[0] * v.Components[1] + _V[1] * v.Components[0];
+      }
+      return new Vector(d);
+    }
+
+    public double Norm()
+    {
+      double sum = 0;
+      for (int i = 0; i < _rows; i++)
+      {
+        sum += _V[i] * _V[i];
+      }
+      return Math.Sqrt(sum);
+    }
+
+    public Vector Normalized()
+    {
+      double[] v = new double[_rows];
+      double norm = this.Norm();
+      for(int i = 0; i < _rows; i++)
+      {
+        v[i] = _V[i] / norm;
+      }
+      return new Vector(v);
+    }
+
+    public String toStr()
+    {
+      return this.toStr(0);
+    }
+
+    public String toStr(int decplcs)
+    {
+      String strout = "";
+
+      for (int i = 0; i < this._rows; i++)
+      {
+        if (decplcs > 0) strout = strout + Math.Round(_V[i], decplcs);
+        else strout = strout + _V[i];
+        strout = strout + "\r\n";
+      }
+
+      return strout;
+    }
+
+    private double fix(double n, double dp)
+    {
+      if (dp > 0)
+      {
+        double prec = Math.Pow(10, dp);
+        double num = Math.Round(n * dp);
+        return num / prec;
+      }
+      return n;
+    }
+  }
+}
